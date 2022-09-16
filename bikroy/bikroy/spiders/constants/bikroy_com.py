ba460be11736bd8f58498b0395106e6dc285b726
Дмитрий Агафонов <d.agafonov@brandquad.ru>
2022-09-16 22:29:03 +0300

    Add useragent and xpath constants

--- bikroy/bikroy/spiders/constants/bikroy_com.py
+++ bikroy/bikroy/spiders/constants/bikroy_com.py
@@ -1,3 +1,8 @@
 DOMAIN = "https://www.bikroy.com"
 
 PART_PRODUCT_PAGE_URL = 'https://bikroy.com/en/ad/'
+
+CATEGORY_URLS_XPATH = "(//div[contains(@id, 'collapsible-content')])[1]//a/@href"
+REGION_URLS_XPATH = "(//div[contains(@id, 'collapsible-content')])[2]//a/@href"
+
+CHROME_USERAGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
