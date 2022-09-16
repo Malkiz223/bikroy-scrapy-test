b55dc20b54fd2b43a16f534837ee1c16f2dc2335
Дмитрий Агафонов <d.agafonov@brandquad.ru>
2022-09-16 22:25:51 +0300

    Add get_url_without_query

--- bikroy/bikroy/spiders/helpers/helpers.py
+++ bikroy/bikroy/spiders/helpers/helpers.py
@@ -1,5 +1,9 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 
 def get_full_link(domain: str, url: str) -> str:
     return urljoin(domain, url)
+
+
+def get_url_without_query(url: str) -> str:
+    return urljoin(url, urlparse(url).path)
