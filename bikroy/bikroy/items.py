a5e5ba9d18ccb89ca5618abbaeb5e1aa1ac13fcf
Дмитрий Агафонов <d.agafonov@brandquad.ru>
2022-09-16 22:25:30 +0300

    Add title and description fields

--- bikroy/bikroy/items.py
+++ bikroy/bikroy/items.py
@@ -4,6 +4,8 @@ from scrapy import Item, Field
 class ProductItem(Item):
     url = Field()
     item_id = Field()
+    title = Field()
+    description = Field()
     creation_timestamp = Field()
     author_name = Field()
     author_phone = Field()
