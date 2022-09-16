3c8d3f58079de7d12846a59263c24334bcac49c0
Дмитрий Агафонов <d.agafonov@brandquad.ru>
2022-09-16 22:25:12 +0300

    Add DuplicatesPipeline

--- bikroy/bikroy/pipelines.py
+++ bikroy/bikroy/pipelines.py
@@ -1,13 +1,16 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
-class BikroyPipeline:
+class DuplicatesPipeline:
+
+    def __init__(self):
+        self.ids_seen = set()
+
     def process_item(self, item, spider):
-        return item
+        adapter = ItemAdapter(item)
+        if adapter['item_id'] in self.ids_seen:
+            raise DropItem(f"Duplicate item found: {item!r}")
+        else:
+            self.ids_seen.add(adapter['item_id'])
+            return item
